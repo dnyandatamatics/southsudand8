ea9c0fcfab4b003cc5aa4c777a543e020cf883eb
dnyandatamatics <dnyaneshwar.kashid@datamatics.com>
2021-03-03 11:35:41 +0530

    bootstrap barrio subtheme added

---
+++ web/themes/custom/crm_2021/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.crm_2021 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
